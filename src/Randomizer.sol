211a864597c928dc6689fea560ac4200986731d6
Dmitry Kozlov <d@kozlov.ru>
2018-07-13 18:57:35 +0300

    Updated rules

--- src/Randomizer.sol
+++ src/Randomizer.sol
@@ -1,62 +1,27 @@
 contract Randomizer {
-    uint256 generatorSeed;
-    address public owner;
-    mapping (address => uint256) private users;
+    uint256 firstNumber;
+    uint256 secondNumber;
+    address owner;
     
-    function Randomizer(string password) public payable {
-        // создаём сид для генерации последующих чисел
-        generatorSeed = uint256(keccak256(password));
-        generatorSeed = random(generatorSeed);
+    // При условии, что первый пользователь это owner
+    function Randomizer(uint256 number) public payable {
+        firstNumber = number;
+        owner = msg.sender;
     }
     
-    function join(string yourPassword) public {
-        require(users[msg.sender] == 0, "You already joined");
-        
-        // Берём хэш пароля и рандомизируем его с generatorSeed
-        uint256 value = uint256(keccak256(yourPassword));
-        value = random(value);
-        value *= generatorSeed;
-        value = random(value);
-        users[msg.sender] = value;
+    function set(uint256 number) public returns (uint256) {
+        require(secondNumber == 0 , "Number already setted");
+        require(msg.sender != owner , "Owner cannot send numbers");
+        require(number != 0 , "You cannot send 0");
+        secondNumber = uint256(keccak256(msg.sender,number));
+        return uint256(keccak256(firstNumber,secondNumber));
     }
-    
-    function numberFor(address user) public returns (uint256) {
-        require(user != msg.sender, "You cannot send your address in address field");
-        require(users[msg.sender] != 0, "Your account not found. Please join first");
-        require(users[user] != 0, "User not found");
-        
-        // Берём числа пользователей. 
-        // Сортируем из через "<" чтобы они были в одинаковом порядке при запросе с обоих сендеров
-        uint256 first;
-        uint256 second;
-        if (msg.sender < user) {
-            first = users[msg.sender];
-            second = users[user];
+    function number() public returns (uint256) {
+        if (msg.sender == owner) {
+            require(secondNumber != 0 , "Someone else should send second number");
         } else {
-            first = users[user];
-            second = users[msg.sender];
+            require(secondNumber != 0 , "You must set second number");
         }
-        
-        // Вычисляем число
-        uint256 result = first;
-        result *= second;
-        result *= generatorSeed;
-        return random(result);
-    }
-    
-    function random(uint256 y) private returns (uint256) {
-        uint256 a = 0x1bea4149536195e7dc498fa7fa68fb435565a867daee79f06029620fa7583313;
-        uint256 b = 0xdc3e381976204eced11873b4e91ed39b9fe62abcee8635047629d392f03d703b;
-        uint256 c = 0x74e5e1b2c6cc6050919374654928b11e17f2919c61067b91a365dfdaa911f4cb;
-        uint256 d = 0x5973bdf32ccce2b9eecc272c0ca94e563020977b4a6246bbd5d80730041b0df8;
-        uint256 e = 0x46a59bb3f902356d5e524d0c486e96f232ba4f4fe79c621ea1ef99749d3dac93;
-        // Рандомизируем число
-        y = (y >> 87) ^ y;
-        y = (y * (y * y * e * a) + b);
-        y = (y * (y * y * c + d) * b);
-        
-        // Умножаем его на сид
-        y *= generatorSeed;
-        return y;
+        return uint256(keccak256(firstNumber,secondNumber));
     }
 }
